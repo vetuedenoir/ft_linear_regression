cc530376a5d5ee84b469732ffa1f636c1adac943
killian <killianscordel@gmail.com>
2024-12-19 01:08:52 +0100

    netoyage, reste commentaire, bonus

---
+++ ft_standard_deviation.py
@@ -0,0 +1,15 @@
+def ft_std(nbs: list[float | int]):
+    """
+Calculate the standard deviation of a list of numbers.
+
+    Args:
+        numbers (List[float | int]): A list of float or integer numbers.
+    """
+
+    if len(nbs) == 0:
+        return print("ERROR")
+    mean = sum(nbs) / len(nbs)
+    ecart = [x - mean for x in nbs]
+    ecart = [x * x for x in ecart]
+    std = (sum(ecart) / len(nbs)) ** 0.5
+    return std
